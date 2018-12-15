c0d3003a8b9cf36af21be2e0b8e2bd9253ae7a3b
Damian Czubski <damianczubski@macbook-pro-damian.home>
2018-12-15 09:41:48 +0100

    Initial commit

---
+++ src/main/java/ex_1_15_12/App.java
@@ -0,0 +1,13 @@
+package ex_1_15_12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
