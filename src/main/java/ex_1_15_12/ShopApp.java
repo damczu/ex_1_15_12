40462fad0354a7bbb07fc9f44538dd432ff0b30b
damczu <dam.czu@interia.pl>
2018-12-15 11:55:24 +0100

    PreparedStatement added

--- src/main/java/ex_1_15_12/ShopApp.java
+++ src/main/java/ex_1_15_12/ShopApp.java
@@ -40,14 +40,28 @@ public class ShopApp {
 
         //query execution
         Statement statement = null;
+        PreparedStatement preparedStatement = null;
 
         try {
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE products " + "SET DESCRIPTION = ? WHERE product_id = ?"
+            );
+            updateProducts(preparedStatement);
+
             statement = connection.createStatement();
             insertProduct(statement);
             performQuery(statement);
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
             try {
                 if (statement != null)
                     statement.close();
@@ -67,10 +81,24 @@ public class ShopApp {
 
     }
 
+    private static void updateProducts(PreparedStatement preparedStatement) {
+        try {
+            preparedStatement.setString(1, "bla");
+            preparedStatement.setInt(2, 1);
+
+            int updated = preparedStatement.executeUpdate();
+
+            System.out.println(updated + " updated products.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void insertProduct(Statement statement) {
         try {
             int inserted = statement.executeUpdate("INSERT INTO products VALUES (1, '123a', 'first product', null)");
-            System.out.println(inserted + "new products added");
+            System.out.println(inserted + " new products added");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -79,15 +107,15 @@ public class ShopApp {
     private static void performQuery(Statement statement) throws SQLException {
         ResultSet resultSet = null;
 
-        try{
+        try {
             resultSet = statement.executeQuery("SELECT product_id, name FROM products");
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String name = resultSet.getString("name");
                 int product_id = resultSet.getInt("product_id");
 
                 System.out.println("Product with id: " + product_id + " and name: " + name + ".");
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
